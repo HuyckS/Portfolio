644137c03054d9716ea735b70dcd6b55a2c86ef0
Spencer Huyck <huycksl@gmail.com>
2021-09-28 13:06:55 -0700

    launch

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PortfolioPro.Models;
+
+namespace PortfolioPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        //for each route this controller is to handle:
+        [HttpGet("")]     //associated route string (exclude the leading /) 
+        public ViewResult Home()
+        {
+            return View("Home");
+        }
+
+        [HttpGet("/resume")]
+        public ViewResult Resume()
+        {
+            return View("Resume");
+        }
+
+        [HttpGet("/projects")]
+        public ViewResult Projects()
+        {
+            return View("Projects");
+        }
+
+        [HttpGet("/skills")]
+        public ViewResult Skills()
+        {
+            return View("Skills");
+        }
+
+        [HttpGet("/contact")]
+        public ViewResult Contact()
+        {
+            return View("Contact");
+        }
+    }
+}
